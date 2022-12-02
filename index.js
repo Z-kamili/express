f163152516b062ea9c9f5fc30526b278405198e7
Z-kamili <zakariakamili97@gmail.com>
2022-12-02 15:17:19 +0000

    fix problem

--- index.js
+++ index.js
@@ -147,10 +147,10 @@ async function createCourse(params) {
 
 async function getCourses() {
     const courses = await Course.find();
-    console.log(courses);
+       return courses;
 }
 
-getCourses();
+// getCourses();
 
 
 
@@ -180,10 +180,10 @@ async function removeCourse(id) {
 
 }
 
-app.get('/',(req,res) => {
-      const courses =  Course.find().sort('name');
+app.get('/',async (req,res) => {
+      const courses = await getCourses();
       console.log(courses);
-      res.render('index',{courses});
+      res.send(courses);
 });
 
 app.get('/api/courses/:id',(req,res) => {
@@ -220,56 +220,56 @@ app.get('/api/courses',(req,res) => {
 
 // });
 
-// app.put('/api/courses/:id',(req,res) => {
+app.put('/api/courses/:id',(req,res) => {
 
-//     // Look up the course
-//     // If not existing, return 404
+    // Look up the course
+    // If not existing, return 404
 
-//     const  course = courses.find(c => c.id === parseInt(req.params.id));
-//     if(!course) return res.status(404).send('The course with the given ID was not found');
+    const  course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) return res.status(404).send('The course with the given ID was not found');
 
-//     // Validate
-//     // If invalid, return 400 - Bad request
+    // Validate
+    // If invalid, return 400 - Bad request
 
-//     const result = ValidateCourse(req.body);
-//     const {error} = ValidateCourse(req.body);
+    const result = ValidateCourse(req.body);
+    const {error} = ValidateCourse(req.body);
 
-//     if(error) return res.status(400).send(result.error.details[0].message);
+    if(error) return res.status(400).send(result.error.details[0].message);
 
-//     course.name = req.body.name;
-//     res.send(course);
-//     //Return the updated course
+    course.name = req.body.name;
+    res.send(course);
+    //Return the updated course
 
 
-// });
+});
 
-// app.delete('/api/courses/:id',(req,res) => {
+app.delete('/api/courses/:id',(req,res) => {
 
-//     //Look up the course
-//     //Not existing, return 404
+    //Look up the course
+    //Not existing, return 404
     
-//     const  course = courses.find(c => c.id === parseInt(req.params.id));
-//     if(!course) return res.status(404).send('The course with the given ID was not found');
+    const  course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) return res.status(404).send('The course with the given ID was not found');
 
-//     //Delete 
-//     const index = courses.indexOf(course);
-//     courses.splice(index,1);
-//     res.send(course);
+    //Delete 
+    const index = courses.indexOf(course);
+    courses.splice(index,1);
+    res.send(course);
 
-//     //Return the same course
+    //Return the same course
 
-// });
+});
 
-// function ValidateCourse(course) {
+function ValidateCourse(course) {
 
-//     //validation
-//     const schema = {
-//         name: Joi.string().min(3).required()
-//     };
+    //validation
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
 
-//     return  Joi.validate(course,schema);
+    return  Joi.validate(course,schema);
 
-// }
+}
 
 const port = process.env.PORT || 3000;
 
